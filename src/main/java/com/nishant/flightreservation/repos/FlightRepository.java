5bf22c90924cc7c71069162eab630f3b74296714
Nishant <nishant3@mail.usf.edu>
2019-04-24 22:04:23 -0400

    commit

--- src/main/java/com/nishant/flightreservation/repos/FlightRepository.java
+++ src/main/java/com/nishant/flightreservation/repos/FlightRepository.java
@@ -1,9 +1,13 @@
 package com.nishant.flightreservation.repos;
 
+import java.util.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nishant.flightreservation.entities.Flight;
 
 public interface FlightRepository extends JpaRepository<Flight, Long> {
+
+	void findFlights(String from, String to, Date departureDate);
 
 }
--- src/main/webapp/WEB-INF/jsps/findFlights.jsp
+++ src/main/webapp/WEB-INF/jsps/findFlights.jsp
@@ -10,5 +10,15 @@
 
 <h2>Find Flights<h2>
 
+
+<form action="findFlights" method="post">
+<pre>
+From:<input type="text" name="from"/>
+To:<input type="text" name="to"/>
+Departure Date:<input type="text" name="departureDate"/>
+<input type="submit" value="Search"/>
+</pre>
+</form>
+
 </body>
 </html>
